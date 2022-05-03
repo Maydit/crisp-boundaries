f0a836cc9c194acbdd5d9aa82ff4cd4426ffe5fb
Maydit <38572912+Maydit@users.noreply.github.com>
2022-05-03 10:23:10 -0400

    update rf to compatible version for 2022

--- Code/util/fastRFreg_predict.m
+++ Code/util/fastRFreg_predict.m
@@ -3,7 +3,7 @@ function [y] = fastRFreg_predict(X,rf)
     scores = zeros(size(X,1),1);
     
     for i=1:rf.Ntrees
-        scores = scores + eval(rf.Trees{i},X);
+        scores = scores + predict(rf.Trees{i},X);
     end
     y = scores./rf.Ntrees;
 end
--- Code/util/fastRFreg_train.m
+++ Code/util/fastRFreg_train.m
@@ -6,7 +6,7 @@ function [rf] = fastRFreg_train(X,y,Ntrees)
     rf.Ntrees = Ntrees;
     
     for i=1:Ntrees
-        t = classregtree(X,y,'method','regression','nvartosample',min(3,size(X,2)),...
+        t = fitrtree(X,y,'nvartosample',min(3,size(X,2)),...
                              'minleaf',5,'prune','off');
         rf.Trees{i} = t;
     end
